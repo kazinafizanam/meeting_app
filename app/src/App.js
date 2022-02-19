c13bcbba418bcc18a477af04fb497d48c98a041b
Kazi Nafiz Anam <99716386+kazinafizanam@users.noreply.github.com>
2022-02-19 11:33:29 +0600

    added

---
+++ app/src/App.js
@@ -0,0 +1,77 @@
+// import meeting from "./meeting.png";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import React, { useState } from "react";
+import axios from "axios";
+
+function App() {
+    const [username, setUsername] = useState("");
+
+    const zoomMeeting = () => {
+        const data = {
+            email: "nafiza.aobs@gmail.com",
+        };
+        console.log(username);
+        axios
+            .post(`http://localhost:3444/meeting`, data)
+            .then((response) => {
+                let URL =
+                    response.data.join_url.replaceAll(
+                        "https://us04web.zoom.us/j/",
+                        "http://localhost:9999/?"
+                    ) + `?role=1?name=${username}`;
+                console.log(URL);
+                window.location.replace(`${URL}`);
+            })
+            .catch((err) => console.error(err));
+    };
+    return (
+        <div className="App">
+            <header className="App-header">
+                {/* <Student /> */}
+                <h1>Zoom Meeting</h1>
+                <div className="card">
+                    <h5>
+                        Name&nbsp;&nbsp;
+                        <input
+                            type="text"
+                            name="name"
+                            style={{
+                                width: "300px",
+                                borderRadius: "5px",
+                                padding: "8px 12px",
+                                fontSize: "18px",
+                            }}
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                        />
+                    </h5>
+
+                    <div className="" style={{ margin: "10px" }}>
+                        <div className="">
+                            <div
+                                className=""
+                                style={{ margin: "10px", marginTop: "120px" }}
+                            >
+                                <button
+                                    className="btn btn-info"
+                                    style={{
+                                        width: "290px",
+                                        height: "80px",
+                                        fontSize: "20px",
+                                        fontFamily: "cursive",
+                                    }}
+                                    onClick={zoomMeeting}
+                                >
+                                    Create Meeting
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </header>
+        </div>
+    );
+}
+
+export default App;
